589a0c9f72a6b910fea3d4b7af4e6363e78533e9
Srikanth Aare <srikanthaare72@icloud.com>
2025-02-06 15:46:07 +0530

    day 2

--- src/main/java/org/example/sliding_window_pattern/LongestSubStringWithDistinctChar.java
+++ src/main/java/org/example/sliding_window_pattern/LongestSubStringWithDistinctChar.java
@@ -1,20 +1,70 @@
 package org.example.sliding_window_pattern;
 
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.*;
 
 public class LongestSubStringWithDistinctChar {
     public static void main(String[] args) {
-        int length = 3;
-        String str = "thinking";
-        int leftIndex = 0;
-        HashSet<Integer> seen = new HashSet<Integer>();
+
+        String str = "AdBBllock";
+//        System.out.println(subStringWithDistinctChar(str));
+//        System.out.println(subStringWithDistinctCharWithSize(str,5));
+        System.out.println(subStringWithDistinctChar2(str));
+
     }
 
-    private static String longestSubString(HashSet hashSet,int lengthOfSubString){
-        for(int rightIndex = 0; rightIndex < hashSet.size();rightIndex++){
-
+    private static int subStringWithDistinctChar(String str) {
+        int maxLength = 0;
+        int leftIndex = 0;
+        int rightIndex = 0;
+        HashSet<Character> seen = new HashSet<Character>();
+        for(rightIndex = 0; rightIndex < str.length();rightIndex++){
+            while(seen.contains(str.charAt(rightIndex))){
+                seen.remove(str.charAt(leftIndex));
+                leftIndex ++;
+            }
+            seen.add(str.charAt(rightIndex));
         }
-        return null;
+        maxLength = Math.max(maxLength, rightIndex - leftIndex + 1);
+        System.out.println(str.substring(leftIndex,rightIndex));
+        System.out.println("leftIndex: " + leftIndex + " rightIndex: " + rightIndex);
+        return maxLength;
+    }
+
+    private static String subStringWithDistinctCharWithSize(String str, int lengthOfSubString) {
+        int leftIndex = 0;
+        int rightIndex = 0;
+        HashSet<Character> seen = new HashSet<Character>();
+        for(rightIndex = 0; rightIndex < str.length();rightIndex++){
+            while(seen.contains(str.charAt(rightIndex))){
+                seen.remove(str.charAt(leftIndex));
+                leftIndex ++;
+            }
+            seen.add(str.charAt(rightIndex));
+            if(lengthOfSubString == seen.size()){
+                break;
+            }
+        }
+        System.out.println("leftIndex: " + leftIndex + " rightIndex: " + rightIndex);
+        return str.substring(leftIndex,rightIndex);
+    }
+
+    private static int subStringWithDistinctChar2(String str){
+        int maxLength = 0;
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int[] freq = new int[128];
+        for(rightIndex = 0; rightIndex < str.length();rightIndex++){
+            char currentChar = str.charAt(rightIndex);
+            freq[currentChar]++;
+            while (freq[currentChar] > 1) {
+                char leftChar = str.charAt(leftIndex);
+                freq[leftChar]--;
+                leftIndex++;
+            }
+        }
+        maxLength = Math.max(maxLength, rightIndex - leftIndex + 1);
+        System.out.println(str.substring(leftIndex,rightIndex));
+        System.out.println("leftIndex: " + leftIndex + " rightIndex: " + rightIndex);
+        return maxLength;
     }
 }
